124e75ca8e01107783e8f6819e866f192392d1a9
Sophie Barstein <136925098+SophieBarstein@users.noreply.github.com>
2025-05-10 14:39:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 import pandas as pd
+import numpy as np
 
 app = FastAPI()
 
-@app.get("/species")
-def get_species_list():
-    return ["c_elegans"]
-
-@app.get("/species/c_elegans")
-def get_c_elegans_data():
-    df = pd.read_csv("c_elegans.csv")
-    return df.to_dict(orient="records")
+@app.get("/species/{name}")
+def get_species(name: str):
+    try:
+        df = pd.read_csv(f"{name.lower()}.csv")
+        df.replace({np.nan: ""}, inplace=True)  # Replace NaNs with empty string
+        return df.to_dict(orient="records")
+    except Exception as e:
+        return {"error": str(e)}
